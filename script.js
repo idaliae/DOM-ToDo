c9c23b34fdfd0bf56ed8d2b842a0237d5fecf747
Idalia Z. Espino Lira <idalia.espino12@gmail.com>
2022-06-20 23:26:40 -0500

    accidental html deletions fixed. Still need to make the strike through/ delete delay.

--- script.js
+++ script.js
@@ -12,13 +12,16 @@ addToDoButton.addEventListener('click', function() {
     const paragraph = document.createElement('p');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
-    // paragraph.style.pageBreakAfter = "always";
     toDoContainer.appendChild(paragraph);
     inputField.value = "";
+    
     paragraph.addEventListener('click', function() {
         paragraph.style.textDecoration = "line-through";
+        
     })
+
     paragraph.addEventListener('dblclick', function() {
-        toDoContainer.removeChild(paragraph)
+        toDoContainer.removeChild(paragraph);
     })
+   
 })
